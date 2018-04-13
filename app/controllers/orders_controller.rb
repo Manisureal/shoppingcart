64ad208c05e5b5ad4b6783b843a4a66bcbf71e7e
Mansoor Zaman <manisureal@outlook.com>
2018-04-13 15:54:09 +0100

    When in Cart the order can now be submitted

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
   def index
-    if !current_order.order_items.empty?
-    @orders = Order.all
-  end
+    # if !current_order.order_items.empty?
+    # if current_order.save
+    # require
+      @orders = Order.all
+      @order_items = current_order.order_items
+    # end
+  # end
   end
 
   def show
     @order = Order.find(params[:id])
     @order_items = @order.order_items
   end
+
+  # only if i want the user to delete the order
+  # then we need to add delete method here
 end
