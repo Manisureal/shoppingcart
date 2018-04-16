21e536fa8f95bae3d64fd2acd326bb059eabb665
Mansoor Zaman <manisureal@outlook.com>
2018-04-16 12:08:24 +0100

    Avatar function now created which displays user initials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_order
   helper_method :reset_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:forname, :surname])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:forname, :surname])
+  end
 
 
   def current_order
