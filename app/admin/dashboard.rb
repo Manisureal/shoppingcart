c024a511956b13d7fd1d14ee13de7c8829c906e5
Mansoor Zaman <manisureal@outlook.com>
2018-04-18 14:08:21 +0100

    Admin Dashboard has now been created

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,34 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Orders" do
+          table_for Order.order("id desc").limit(10) do
+            column("Order#") { |order| link_to(order.id, admin_order_path(order)) }
+            column("Order Date") { |order| order.created_at }
+            column("Status") { |order| status_tag(order.status, class: order.status == "pending" ? "error" : "done") }
+            column("Customer") { |order| link_to(order.user.forname + ' ' + order.user.surname, admin_user_path(order.user)) }
+            column("Total")   { |order| number_to_currency order.total_price }
+          end
+        end
+      end
+
+
+      column do
+        panel "Recent Customers" do
+          table_for User.order("id desc").limit(10).each do |user|
+            column(:customer_name) { |user| user.forname + ' ' + user.surname }
+            column(:customer_email) { |user| link_to(user.email, admin_user_path(user)) }
+            column(:last_signed) { |user| user.last_sign_in_at }
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
+
+
+# Order.all.map do |o|
+#   li link_to(o.user.forname, admin_order_path(o))
+
