afaa27ca1384fe9895bb4744f1fceeee6283c370
Mansoor Zaman <manisureal@outlook.com>
2018-04-19 14:46:37 +0100

    Unused code across the entire project has now been removed

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,13 +3,11 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order.status = "Initiated"
-    # @order.total_price = calculate_total
     @item = @order.order_items.new(item_params)
     authorize @item
     @item.save
     session[:order_id] = @order.id
     @order.user_id = current_user.id
-    # require
     redirect_to products_path
   end
 
@@ -20,14 +18,9 @@ class OrderItemsController < ApplicationController
     redirect_to cart_path
   end
 
-  # def calculate_total
-  #   @order_items.map { |item| item.product.price * item.quantity.to_i }.sum
-  # end
-
   private
 
   def item_params
     params.require(:order_item).permit(:quantity, :product_id)
   end
-
 end
