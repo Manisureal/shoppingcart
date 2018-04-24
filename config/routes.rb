2e001b86b294bcb197c8ed4f1a0edfdf7117ab53
Mansoor Zaman <manisureal@outlook.com>
2018-04-24 12:01:13 +0100

    Admin Signout Custom Route now added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root to: 'pages#home'
   resources :products, only: [:index, :show]
   resources :order_items
