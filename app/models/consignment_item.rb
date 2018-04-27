140eb0f75ec1b0c1ac17101e3522acdda8560e1a
Mansoor Zaman <manisureal@outlook.com>
2018-04-27 15:28:10 +0100

    Created a method to calculate the total for consignment items

--- app/models/consignment_item.rb
+++ app/models/consignment_item.rb
@@ -1,4 +1,10 @@
 class ConsignmentItem < ApplicationRecord
   belongs_to :consignment
   belongs_to :order_item
+  validates :consignment_items, presence: :true
+
+  def consign_total
+    # self.consignment.order.order_items.map { |qd| qd.quantity_dispatched }.sum
+    self.consignment.consignment_items.map { |ci| ci.quantity }.sum
+  end
 end
