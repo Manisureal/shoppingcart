317423258162c65a50ad1fdc0063b37798003cc5
Mansoor Zaman <manisureal@outlook.com>
2018-05-04 15:48:34 +0100

    Admin products has a custom layout now

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Product do
-  permit_params :name, :price, :description, :pack_size, :product_code
+  permit_params :price, :description, :pack_size, :product_code
+
+  index do
+    selectable_column
+    column :id do |p|
+      link_to p.id, admin_product_path(p)
+    end
+    column :price do |p|
+      number_to_currency p.price
+    end
+    column :description
+    column :pack_size
+    column :product_code
+    actions
+  end
 end
