8026b82158dd62850a8947e1114bb1699aa0783e
Mansoor Zaman <manisureal@outlook.com>
2018-05-16 09:59:19 +0100

    Added Jquery search for Companies Admin

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Company do
   permit_params :name, :address, :postcode, :contact_name, :phone, :fax, :email, :xero_id
+
+  filter :name, as: :select, label: "Search by Name", prompt: "Select or Type", collection: Company.all.collect { |c| c.name }, input_html: { class: 'chosen-select2' }
+  filter :address, as: :select, label: "Search by Address", prompt: "Select or Type", collection: Company.all.collect { |c| c.address }, input_html: { class: 'chosen-select2' }
+  filter :postcode, as: :select, label: "Search by Postcode", prompt: "Select ot Type", collection: Company.all.collect { |c| c.postcode }, input_html: { class: 'chosen-select2' }
+  filter :contact_name, as: :select, label: "Search by Contact Name", prompt: "Select or Type", collection: Company.all.collect { |c| c.postcode}, input_html: { class: 'chosen-select2' }
+  filter :phone, as: :select, label: "Search by Phone Number", prompt: "Select or Type", collection: Company.all.collect { |c| c.phone }, input_html: { class: 'chosen-select2' }
+  filter :email, as: :select, label: "Search by Email", prompt: "Select or Type", collection: Company.all.collect { |c| c.email }, input_html: { class: 'chosen-select2' }
+
 end
