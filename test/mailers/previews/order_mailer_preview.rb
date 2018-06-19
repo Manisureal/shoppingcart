ee6314134eeabd91dcdaeb9cd0bae257f1ee57fb
Mansoor Zaman <manisureal@outlook.com>
2018-06-19 11:10:47 +0100

    initialized order mailer and relevant files

---
+++ test/mailers/previews/order_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/order_mailer
+class OrderMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/order_mailer/order_submitted
+  def order_submitted
+    OrderMailer.order_submitted
+  end
+
+end
