9a18e99a20f1333c28d52394a4071a228b752339
Mansoor Zaman <manisureal@outlook.com>
2018-07-02 16:32:25 +0100

    Fixed My Profile Link in Active Admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -224,7 +224,7 @@ ActiveAdmin.setup do |config|
   #
     config.namespace :admin do |admin|
       admin.build_menu :utility_navigation do |menu|
-        menu.add label: "My Great Website", url: "/admin/staff/#{User.last.id}"#, html_options: { target: :blank }
+        menu.add label: "My Profile", url: "/admin/staffs/#{User.last.id}"#, html_options: { target: :blank }
         admin.add_logout_button_to_menu menu
       end
     end
