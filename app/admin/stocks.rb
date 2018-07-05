ec8ce1670f788293026e90472d76acdaf3fbc728
Mansoor Zaman <manisureal@outlook.com>
2018-07-05 16:33:33 +0100

    Admin Model files modified and stock migration file

--- app/admin/stocks.rb
+++ app/admin/stocks.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Stock do
+  belongs_to :product
+  permit_params :stock_change, :stock_message
 
-
+  form do |f|
+    f.inputs do
+      f.input :product, collection: Product.all.map { |p| [p.description, p.id] }, input_html: { class: 'chosen-select', disabled: true }
+      f.input :stock_change
+      f.input :stock_message, as: :select, collection: ["Delivery", "Returned", "Stock Check"], input_html: { class: 'chosen-select' }
+    end
+    actions
+  end
 end
