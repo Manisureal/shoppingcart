65c7a3ecec9fa89325550e080fdaf0f07c56c7b6
Mansoor Zaman <manisureal@outlook.com>
2018-07-05 16:47:36 +0100

    Custom Methods now created in Models

--- app/models/consignment_item.rb
+++ app/models/consignment_item.rb
@@ -1,9 +1,22 @@
 class ConsignmentItem < ApplicationRecord
   belongs_to :consignment
   belongs_to :order_item
+  has_one :stock
+  before_save :do_before_save
 
   def consign_total
     # self.consignment.order.order_items.map { |qd| qd.quantity_dispatched }.sum
     self.consignment.consignment_items.map { |ci| ci.quantity }.sum
+  end
+
+  private
+
+  def do_before_save
+    self.stock = Stock.new unless self.stock
+    self.stock.product = self.order_item.product
+    self.stock.sale_price = self.order_item.product.price
+    self.stock.product_description = self.order_item.product.description
+    self.stock.stock_change = 0 - self.quantity
+    self.stock.stock_message = "Dispatched Stock"
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :order_items
+  has_many :stocks
   audited
+
+  def current_stock
+    self.stocks.sum(:stock_change)
+  end
 end
