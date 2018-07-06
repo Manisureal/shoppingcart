a84f5a0bbad9fbe01b11c75ec4cae56b19c81e56
Mansoor Zaman <manisureal@outlook.com>
2018-07-06 12:12:49 +0100

    removed cost_price field from stocks index

--- app/admin/stocks.rb
+++ app/admin/stocks.rb
@@ -2,6 +2,18 @@ ActiveAdmin.register Stock do
   belongs_to :product
   permit_params :stock_change, :stock_message
 
+  index do
+    selectable_column
+    columns_to_exclude = ["cost_price","updated_at"]
+    (Stock.column_names - columns_to_exclude).each do |c|
+      column c.to_sym
+    end
+    actions
+    # column :description do
+    #   raw "<a class='view_description button'>View Description</a>"
+    # end
+  end
+
   form do |f|
     f.inputs do
       f.input :product, collection: Product.all.map { |p| [p.description, p.id] }, input_html: { class: 'chosen-select', disabled: true }
