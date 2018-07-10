61c1c8553b0d95463795670fe1c22a245eb3f4b4
Mansoor Zaman <manisureal@outlook.com>
2018-07-10 14:07:35 +0100

    Custom search created for stock search in admin dashboard

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,10 @@ class Product < ApplicationRecord
   has_many :stocks
   audited
 
+  def stock_count_on(date)
+    self.stocks.where('created_at <= ?', date.to_time.end_of_day).sum(:stock_change)
+  end
+
   def current_stock
     self.stocks.sum(:stock_change)
   end
