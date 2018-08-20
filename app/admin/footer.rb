aef71dc9bb47c49a00b0b83b55036e5bdd79a691
Mansoor Zaman <manisureal@outlook.com>
2018-08-20 14:20:49 +0100

    Disabled footer

--- app/admin/footer.rb
+++ app/admin/footer.rb
@@ -7,7 +7,7 @@ module ActiveAdmin
         super :style => "font-size:1.5em;color:#696969;"
 
         div do
-          small "Created with love at Caremeds - #{Date.today.year}"
+          # small "Created with love at Caremeds - #{Date.today.year}"
         end
       end
 
