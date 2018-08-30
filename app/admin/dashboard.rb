5485b9100d352f45584e3d9d5125b0cb4aa775ee
Mansoor Zaman <manisureal@outlook.com>
2018-08-30 14:31:52 +0100

    Admin Dashboard is now much more responsive than before

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,38 +3,38 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    columns class: "col-adjustments" do
+    columns class: "cols-adjust" do
       column do
-        panel "Recent Orders", class: "orders" do
+        panel "RECENT ORDERS", class: "orders" do
           table_for Order.where(status: ["Ordered", "Incomplete"]).order("id desc").limit(10) do
             column("Order#") { |order| link_to(order.id, admin_order_path(order)) }
-            column("Order Date") { |order| order.created_at }
-            column("Order Update Date") { |order| (order.updated_at == order.created_at) ? "Not Updated" : order.updated_at }
+            column("Ordered Date") { |order| order.created_at }
+            column("Updated Date") { |order| (order.updated_at == order.created_at) ? "Not Updated" : order.updated_at }
             column("Status") { |order| status_tag(order.status, class: order.status == "Ordered" ? "error" : "warning") }
             # column("Customer") { |order| link_to(order.user.forname.to_s + ' ' + order.user.surname.to_s, admin_customer_path(order.user)) }
             column("Customer") { |order| link_to(order.company.name.to_s, admin_company_path(order.company)) }
             # require
             column("Total")   { |order| number_to_currency order.total_price }
-            column("Action") { |order| link_to("Take Order", admin_order_path(order) + "?take_order=true" ) }
+            column() { |order| link_to( icon('check'), admin_order_path(order) + "?take_order=true" ) }
           end
         end
       end
 
       column do
-        panel "Missing Tracking No" do
+        panel "MISSING TRACKING NOs" do
           render partial: "tracking_no_form"
         end
       end
 
     end
 
-    columns class: "col-adjustments" do
+    columns class: "cols-adjust" do
 
       column do
-        panel "Stock Levels" do
+        panel "STOCK LEVELS" do
           table_for Product.all.limit(12).each do |product|
             column(:product_code) { |product| link_to(product.product_code, admin_product_path(product))}
-            column(:description) { |product| link_to(product.description, admin_product_path(product))}
+            column(:description) { |product| link_to(product.description.truncate(42), admin_product_path(product))}
             column(:pack_size)
             column(:price) { |product| number_to_currency product.price}
             column(:current_stock) { |product| label(product.current_stock,
@@ -42,13 +42,13 @@ ActiveAdmin.register_page "Dashboard" do
                (product.current_stock == 0 || product.current_stock < 0) ? "status bad-level" :
                (product.current_stock.to_i <= product.minimum_stock.to_i) ? "status okay-level" : nil)
             }
-            column() { |p| link_to("Add Stock", new_admin_product_stock_path(p))}
+            column() { |p| link_to icon('plus'), new_admin_product_stock_path(p) }
           end
         end
       end
 
       column do
-        panel "Recent Customers" do
+        panel "RECENT CUSTOMERS" do
           table_for User.where(admin: false).order("id desc").limit(10).each do |user|
 
             column(:customer_name) { |user| (user.forname ? user.forname : 'Unknown') + ' ' + (user.surname ? user.surname : 'User')}
@@ -60,9 +60,9 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    columns class: "col-adjustments" do
+    columns class: "cols-adjust" do
       column do
-        panel "Stock Count" do
+        panel "STOCK COUNT" do
           render 'date_range_search'
         end
       end
