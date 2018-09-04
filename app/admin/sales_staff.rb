f2206f72e9dca5b33796cb7a085f07f3fef3dbe8
Mansoor Zaman <manisureal@outlook.com>
2018-09-04 17:00:25 +0100

    Added Sales Staff to Users in Admin

---
+++ app/admin/sales_staff.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register User, as: "Sales Staff" do
+  permit_params :forname, :surname, :email, :password, :password_confirmation, :sales, :company_id
+
+  menu parent: "Users"
+
+  controller do
+    def scoped_collection
+     end_of_association_chain.where(sales: true)
+    end
+  end
+
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :forname
+    column :surname
+    column :created_at
+    column :admin
+    column :sales
+    column :company
+    actions
+  end
+
+  form do |f|
+    f.inputs "Identity" do
+      f.input :company
+      f.input :email
+      f.input :forname
+      f.input :surname
+      f.input :password
+    end
+    f.inputs "Sales" do
+      f.input :sales
+    end
+    f.actions
+  end
+
+end
+
