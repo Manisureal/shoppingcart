bdcd3b49b96a70815e30c8c9905e9e947cad4ed0
Mansoor Zaman <manisureal@outlook.com>
2018-09-07 15:23:07 +0100

    enabled site title for admins

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -24,7 +24,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  # config.site_title = "Caremeds Order Management System"
+  config.site_title = "Caremeds Order Management System"
   # config.authorization_adapter = "SalesDashboardAuthorization"
 
   # config.authorization_adapter = ActiveAdmin::PunditAdapter
