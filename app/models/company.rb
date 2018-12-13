01c7ce735d199bfbe3add4f99d162522066eec1c
Mansoor Zaman <manisureal@outlook.com>
2018-12-13 09:22:30 +0000

    validated email for company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :orders
   has_many :consignments, through: :orders
-  validates :email, presence: true
+  validates :email, format: { with: /\A.*@.*\.com\z/ }, presence: true
   audited
 
   # geocoded_by :geocoder_address
